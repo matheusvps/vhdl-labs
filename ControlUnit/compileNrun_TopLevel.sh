51205694b2aa683107fdea5d09f5162502a0efa3
Jhouny <jhonyaraujo9121@gmail.com>
2024-11-27 15:26:31 -0300

    Moved file locations

--- ControlUnit/compileNrun_TopLevel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ghdl -a *.vhd
-ghdl -e ProgramCounter, ProgramCounter_Control, ROM, StateMachine, ControlUnit, TopLevel_tb
-
-ghdl -r TopLevel_tb --wave=TopLevel_tb_wave.ghw
-
-gtkwave TopLevel_tb_wave.ghw -o TopLevel_tb_wave_config.gtkw
-read  -n 1
